2e7ba47c1545e5813111ae7945a1d95786f01743
Tiep Tran <t-tran@dimage.co.jp>
2020-04-14 16:52:15 +0700

    creat and append dataframe

---
+++ delection.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df = pd.DataFrame([[1, 2], [3, 4]], columns = ['a','b'])
+df2 = pd.DataFrame([[5, 6], [7, 8]], columns = ['a','b'])
+
+df = df.append(df2)
+
+# Drop rows with label 0
+df = df.drop(0)
+
+print df
