08ccbac2e3efb8df4db7a6aa3505e62fefe8e5f6
Tiep Tran <t-tran@dimage.co.jp>
2020-04-14 16:45:53 +0700

    creat and append dataframe

---
+++ append.py
@@ -0,0 +1,11 @@
+
+# Importing pandas as pd 
+import pandas as pd 
+  
+df1 = df = pd.DataFrame({"a":[1, 2, 3, 4], 
+                         "b":[5, 6, 7, 8]}) 
+df2 = pd.DataFrame({"a":[1, 2, 3], 
+                    "b":[5, 6, 7]}) 
+  
+print(df1, "\n")
+df2 
---
+++ created.py
@@ -0,0 +1,8 @@
+import pandas as pd 
+
+# list of strings 
+lst = ['tiep test 1', 'tiep test 2', 'tiep test 1', 'tiep test 3',  
+            'tiep test 4', 'tiep test 2', 'tiep test 5'] 
+  
+df = pd.DataFrame(lst) 
+print(df) 
